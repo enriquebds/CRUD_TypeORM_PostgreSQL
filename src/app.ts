36add4b6a3186bd88826cb401c3829b96be411e2
Enrique <enrique.barbosasilva@gmail.com>
2022-10-14 18:21:23 -0300

    refactor: code-changed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-import "reflect-metadata"
-import express from "express"
+import "reflect-metadata";
+import express from "express";
+import userRoutes from "./routes/user.routes";
+import userLogin from "./routes/login.routes";
 
+const app = express();
+app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/login", userLogin);
 
+app.listen(3334, () => {
+  console.log("Server running in port 3334");
+});
 
-const app = express()
-app.use(express.json())
-
-
-export default app
+export default app;
