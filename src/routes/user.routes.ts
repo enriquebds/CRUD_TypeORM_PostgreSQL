a501a9c16fea97c21bcf7b06e2006fa937325456
Enrique <enrique.barbosasilva@gmail.com>
2022-10-15 15:06:10 -0300

    feat: updateUser-route-created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import createUserController from "../controllers/user/createUser.controller";
 import listUsersController from "../controllers/user/listUsers.controller";
+import updateUserController from "../controllers/user/updateUser.controller";
 import tokenAuthMiddleware from "../middleware/tokenAuth.middleware";
 import verifyIsAdmMiddleware from "../middleware/verifyIsAdm.middleware";
 
@@ -8,5 +9,11 @@ const routes = Router();
 
 routes.post("", createUserController);
 routes.get("", tokenAuthMiddleware, verifyIsAdmMiddleware, listUsersController);
+routes.patch(
+  "/:id",
+  tokenAuthMiddleware,
+  verifyIsAdmMiddleware,
+  updateUserController
+);
 
 export default routes;
