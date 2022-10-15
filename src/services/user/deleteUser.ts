cdc579b4681f6028600307afbed48757fd20927a
Enrique <enrique.barbosasilva@gmail.com>
2022-10-15 16:40:19 -0300

    feat: deleteUser-service-created

---
+++ src/services/user/deleteUser.ts
@@ -0,0 +1,19 @@
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
+
+const deleteUser = async (id: string) => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const user = await userRepository.findOneBy({ id });
+
+  if (!user?.isActive) {
+    throw new Error("This account are disabled");
+  }
+
+  user.isActive = false;
+  await userRepository.save(user);
+
+  return true;
+};
+
+export default deleteUser;
