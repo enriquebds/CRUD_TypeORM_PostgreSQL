cf415ee8ac69f9f51771412ffba12dd46cee87c2
Enrique <enrique.barbosasilva@gmail.com>
2022-10-15 16:40:41 -0300

    feat: deleteUser-controller-created

---
+++ src/controllers/user/deleteUser.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import deleteUser from "../../services/user/deleteUser";
+
+const deleteUserController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await deleteUser(id);
+
+    return res.status(204).json({ message: "User deleted!" });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+  }
+};
+
+export default deleteUserController;
