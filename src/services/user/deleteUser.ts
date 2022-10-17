ce130128f54cfc375285c900014944e2de11bc68
Enrique <enrique.barbosasilva@gmail.com>
2022-10-17 14:35:04 -0300

    refactor: deleteUser-service-changed

--- src/services/user/deleteUser.ts
+++ src/services/user/deleteUser.ts
@@ -1,19 +1,30 @@
 import AppDataSource from "../../data-source";
 import { User } from "../../entities/user.entity";
+import { IUserRequest } from "../../interfaces/users";
 
-const deleteUser = async (id: string): Promise<boolean> => {
+const deleteUser = async (
+  id: string
+): Promise<User | Array<string | number>> => {
   const userRepository = AppDataSource.getRepository(User);
 
   const user = await userRepository.findOneBy({ id });
 
+  if (!user) {
+    return ["User not found", 404];
+  }
+
   if (!user?.isActive) {
-    throw new Error("This account are disabled");
+    return ["This account are disabled", 400];
   }
 
   user.isActive = false;
   await userRepository.save(user);
 
-  return true;
+  const returningUser = await userRepository.findOneBy({
+    id,
+  });
+
+  return returningUser!;
 };
 
 export default deleteUser;
