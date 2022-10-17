5e5a4f5afd7dc6cfd91c6eacac88bb1a2613cb54
Enrique <enrique.barbosasilva@gmail.com>
2022-10-17 14:36:27 -0300

    refactor: deleteUser-controller-changed

--- src/controllers/user/deleteUser.controller.ts
+++ src/controllers/user/deleteUser.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
+import { User } from "../../entities/user.entity";
 import deleteUser from "../../services/user/deleteUser";
 
 const deleteUserController = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    await deleteUser(id);
+    const deletedUser = await deleteUser(id);
 
-    return res.status(204).json({ message: "User deleted!" });
+    if (deletedUser instanceof User) {
+      return res.status(204).json(deletedUser);
+    }
+    return res.status(deletedUser[1] as number).json({
+      message: deletedUser[0],
+    });
   } catch (error) {
     if (error instanceof Error) {
       return res.status(404).json({
