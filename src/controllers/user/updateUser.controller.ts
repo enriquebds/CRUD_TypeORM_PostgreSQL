1891397b8785088162edf352f03a83b4061ea8c7
Enrique <enrique.barbosasilva@gmail.com>
2022-10-17 13:24:44 -0300

    refactor: updateUser-controller-changed

--- src/controllers/user/updateUser.controller.ts
+++ src/controllers/user/updateUser.controller.ts
@@ -8,6 +8,7 @@ const updateUserController = async (req: Request, res: Response) => {
     const id: string = req.params.id;
 
     const updatedUser = await updateUser(user, id);
+
     return res.status(200).json({
       message: "User updated",
       user: updatedUser,
